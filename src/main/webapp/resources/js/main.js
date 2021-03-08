4d389acdfb0cba2ee270000c8c63bda7a4109360
Minus960703 <55120608+Minus960703@users.noreply.github.com>
2021-03-08 22:59:21 +0900

    starbuck 1hour

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -17,3 +17,26 @@ searchInputEl.addEventListener('blur', function () { /*포커스 해제하면 bl
     searchInputEl.setAttribute('placeholder', ''); /*html의 속성을 attribute라 함*/
     /*속성 이름 , 넣을 내용*/
 });
+
+const badgeEl = document.querySelector('header .badges'); /*header 안에 badges 찾기*/
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if(window.scrollY > 500){   //스크롤이 아래로 가면 뱃지 삭제
+        //배지 숨기기
+        // badgeEl.style.display = 'none';
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity: 0,      //뱃지를 0.6초동안 애니메이션 처리
+            display: 'none'
+        }); //0.6초
+    }else{
+        //배지 보이기
+        // badgeEl.style.display = 'block';
+        gsap.to(badgeEl, .6, {
+            opacity: 1,      //뱃지를 0.6초동안 애니메이션 처리
+            display: 'block'
+        }); //0.6초
+    }
+}, 300)); /*300 = 0.3초 0.3초단위로 부하를 줘서 함수 실행되는거 방지*/
+// _.throttle(함수, 시간)
